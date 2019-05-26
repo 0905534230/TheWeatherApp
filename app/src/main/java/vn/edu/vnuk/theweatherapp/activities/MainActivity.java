51e094dde3b0c79d0cc4269c6127a94cc30fedb1
tuannguyencorei7 <tuannguyencorei7@gmail.com>
2019-05-26 19:38:48 +0700

    add

--- app/src/main/java/vn/edu/vnuk/theweatherapp/activities/MainActivity.java
+++ app/src/main/java/vn/edu/vnuk/theweatherapp/activities/MainActivity.java
@@ -6,13 +6,13 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -35,8 +35,10 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import vn.edu.vnuk.theweatherapp.models.Weather;
 import vn.edu.vnuk.theweatherapp.utils.Formatting;
+import vn.edu.vnuk.theweatherapp.utils.UI;
+import vn.edu.vnuk.theweatherapp.activities.BaseActivity;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener, LocationListener {
 
     private FirebaseAuth auth;
@@ -100,6 +102,11 @@ public class MainActivity extends AppCompatActivity
 
         // Initiate activity
         super.onCreate(savedInstanceState);
+        appView = findViewById(R.id.viewApp);
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        AppBarLayout appBarLayout = findViewById(R.id.appBarLayout);
+
+        progressDialog = new ProgressDialog(MainActivity.this);
         setContentView( R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
